47060a9f5855b19af03c01885d6c5ab2a580a565
Bhargav-611 <bhargav.chalodiya611@gmail.com>
2025-09-10 18:01:47 +0530

    solve bootstrap error

--- project1/settings.py
+++ project1/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'jet_django',
-    'bootstrap5',
+    'django_bootstrap5',
     'registration',
     'admin_app',
     'faculty_app',
